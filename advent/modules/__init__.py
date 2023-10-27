bd62ffb0a89f41fe9b84cdd761b040d1226afeb5
MetGang <patrick_stritch@wp.pl>
2023-10-27 23:21:59 +0200

    Update/fix package structure

---
+++ advent/modules/__init__.py
@@ -0,0 +1,12 @@
+from . import (algorithms, combinators, functions, generators, operators,
+               regex, textual)
+
+__all__ = [
+    'algorithms',
+    'combinators',
+    'functions',
+    'generators',
+    'operators',
+    'regex',
+    'textual',
+]
