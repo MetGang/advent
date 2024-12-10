e0d84b30b53c31f8a8e4a7d6497e72c836cbba75
MetGang <patrick_stritch@wp.pl>
2024-12-10 19:06:07 +0100

    Update

--- advent/modules/combinators.py
+++ advent/modules/combinators.py
@@ -1,3 +1,8 @@
+import inspect as __inspect
+from typing import Any as __Any
+from typing import Callable as __Callable
+from typing import Union as __Union
+
 from ..classes import AnyFn as __AnyFn
 from ..classes import BinaryFn as __BinaryFn
 from ..classes import NullaryFn as __NullaryFn
@@ -7,13 +12,15 @@ __all__ = [
     'train',
 ]
 
-def train(f: __AnyFn, g: __BinaryFn, h: __AnyFn) -> __AnyFn:
+def train(f: __Union[__Callable[[], __Any], __Callable[[__Any], __Any], __Callable[[__Any, __Any], __Any]], g: __Callable[[__Any, __Any], __Any], h: __Union[__Callable[[], __Any], __Callable[[__Any], __Any], __Callable[[__Any, __Any], __Any]]) -> __AnyFn:
     """Apply results of `f` and `h` to the binary function `g`"""
-    match [ f, h ]:
-        case [ __NullaryFn(), __NullaryFn() ]:
+    f_arity = len(__inspect.signature(f).parameters)
+    h_arity = len(__inspect.signature(h).parameters)
+    match [ f_arity, h_arity ]:
+        case [ 0, 0 ]:
             return __NullaryFn(lambda: g(f(), h()))
-        case [ __UnaryFn(), __UnaryFn() ]:
+        case [ 1, 1 ]:
             return __UnaryFn(lambda a: g(f(a), h(a)))
-        case [ __BinaryFn(), __BinaryFn() ]:
+        case [ 2, 2 ]:
             return __BinaryFn(lambda a, b: g(f(a, b), h(a, b)))
     return None
