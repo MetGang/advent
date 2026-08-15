f95dc3e4d110e52770eed4bbed5676b3a73ec9c4
MetGang <patrick_stritch@wp.pl>
2026-08-15 13:49:32 +0200

    regex module overhaul

--- advent/modules/regex.py
+++ advent/modules/regex.py
@@ -1,21 +1,49 @@
-import re as __re
+import re
+from typing import Any
 
-from ..classes import UnaryFn as __UnaryFn
+from ..classes import UnaryFn
 
-def split(pattern: str, limit: int = 0) -> __UnaryFn:
-    """_"""
-    def __inner(s: str) -> str:
-        return __re.split(pattern, s, limit)
-    return __UnaryFn(__inner)
+__all__ = [
+    'split',
+    'replace',
+    'find_all',
+    'find_first',
+    'find_last',
+]
 
-def replace(pattern: str, new: str) -> __UnaryFn:
+def split(pattern: str | re.Pattern[str], limit: int = 0) -> UnaryFn:
+    """Return list of substrings split by occurrences matching `pattern`"""
+    def __inner(s: str) -> list[str | Any]:
+        return re.split(pattern, s, limit)
+    return UnaryFn(__inner)
+
+def replace(pattern: str | re.Pattern[str], new: str) -> UnaryFn:
     """Return string with all occurrences matching `pattern` replaced by `new`"""
     def __inner(s: str) -> str:
-        return __re.sub(pattern, new, s)
-    return __UnaryFn(__inner)
+        return re.sub(pattern, new, s)
+    return UnaryFn(__inner)
 
-def find_all(pattern: str) -> __UnaryFn:
-    """_"""
-    def __inner(s: str) -> str:
-        return __re.findall(pattern, s)[0]
-    return __UnaryFn(__inner)
+def find_all(pattern: str | re.Pattern[str]) -> UnaryFn:
+    """Return list of all occurrences matching `pattern`"""
+    def __inner(s: str) -> list[Any]:
+        return re.findall(pattern, s)
+    return UnaryFn(__inner)
+
+def find_first(pattern: str | re.Pattern[str]) -> UnaryFn:
+    """Return first occurrence matching `pattern`, or None if not found"""
+    def __inner(s: str) -> Any:
+        match = re.search(pattern, s)
+        if not match:
+            return None
+        groups = match.groups()
+        if not groups:
+            return match.group(0)
+        return groups[0] if len(groups) == 1 else groups
+    return UnaryFn(__inner)
+
+def find_last(pattern: str | re.Pattern[str]) -> UnaryFn:
+    """Return last occurrence matching `pattern`, or None if not found"""
+    def __inner(s: str) -> Any:
+        matches = re.findall(pattern, s)
+        return matches[-1] if matches else None
+    return UnaryFn(__inner)
