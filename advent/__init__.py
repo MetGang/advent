e05579a111c22ac346d37d0198653f2601cb2e20
MetGang <patrick_stritch@wp.pl>
2026-08-15 15:03:10 +0200

    Rename shortened modules

--- advent/__init__.py
+++ advent/__init__.py
@@ -1,7 +1,7 @@
 from .modules import algorithms as al
 from .modules import combinators as cb
-from .modules import functions as fun
-from .modules import generators as gen
+from .modules import functions as fn
+from .modules import generators as gn
 from .modules import operators as op
-from .modules import regex as rgx
-from .modules import textual as txt
+from .modules import regex as rx
+from .modules import textual as tt
