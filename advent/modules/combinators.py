233c4a9d0b8d7d53bb00970032227ddbe4205d1c
MetGang <patrick_stritch@wp.pl>
2026-08-15 14:34:59 +0200

    combinators module overhaul

--- advent/modules/combinators.py
+++ advent/modules/combinators.py
@@ -1,22 +1,23 @@
-import inspect as __inspect
-from typing import Any as __Any
-from typing import Callable as __Callable
-from typing import Union as __Union
+import inspect
+from typing import Any, Callable
 
-from ..classes import AnyFn as __AnyFn
-from ..classes import BinaryFn as __BinaryFn
-from ..classes import NullaryFn as __NullaryFn
-from ..classes import UnaryFn as __UnaryFn
+from ..classes import AnyFn, BinaryFn, NullaryFn, UnaryFn
 
-def train(f: __Union[__Callable[[], __Any], __Callable[[__Any], __Any], __Callable[[__Any, __Any], __Any]], g: __Callable[[__Any, __Any], __Any], h: __Union[__Callable[[], __Any], __Callable[[__Any], __Any], __Callable[[__Any, __Any], __Any]]) -> __AnyFn:
+__all__ = [
+    'train'
+]
+
+def train(f: Callable[..., Any], g: Callable[[Any, Any], Any], h: Callable[..., Any]) -> AnyFn:
     """Apply results of `f` and `h` to the binary function `g`"""
-    f_arity = len(__inspect.signature(f).parameters)
-    h_arity = len(__inspect.signature(h).parameters)
-    match [ f_arity, h_arity ]:
-        case [ 0, 0 ]:
-            return __NullaryFn(lambda: g(f(), h()))
-        case [ 1, 1 ]:
-            return __UnaryFn(lambda a: g(f(a), h(a)))
-        case [ 2, 2 ]:
-            return __BinaryFn(lambda a, b: g(f(a, b), h(a, b)))
-    return None
+    f_arity = len(inspect.signature(f).parameters)
+    h_arity = len(inspect.signature(h).parameters)
+
+    match [f_arity, h_arity]:
+        case [0, 0]:
+            return NullaryFn(lambda: g(f(), h()))
+        case [1, 1]:
+            return UnaryFn(lambda a: g(f(a), h(a)))
+        case [2, 2]:
+            return BinaryFn(lambda a, b: g(f(a, b), h(a, b)))
+        case _:
+            raise ValueError(f'Unsupported or mismatched arities for f ({f_arity}) and h ({h_arity})')
